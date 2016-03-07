cf9a4f373d6f1d529df5f9915465631af080c008
telephoneman <telephoneman@riseup.net>
2016-03-07 01:14:38 -0600

    Update contact1.php

--- contact1.php
+++ contact1.php
@@ -19,8 +19,63 @@ if($_POST["submit"]) {
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Contact form to email</title>
     
+    <style>
+	
+		body	{ width:960px; padding:100px 0; margin:0 auto; font-family:Calibri, sans-serif; }
+		#nav{
+			border:1px solid #000;
+			border-width:1px 0;
+			list-style:none;
+			margin:0;
+			padding:0;
+			text-align:center;
+		}
+		#nav li{
+			display:inline;
+		}
+		#nav a{
+			display:inline-block;
+			padding:10px;
+		}
+		a{
+			color:#c00;
+			text-decoration:none;
+			font-weight:bold;
+		}
+		a:hover{
+			text-decoration:underline;
+		}
+		
+	</style>
+    
+    <title>Contact form to email</title>
+</head>
+
+<body>
+	
+	<ul id="nav">
+	
+		<li><a href="http://seedsfeedsandneeds.com/index1.html">Home</a></li>
+		<li><a href="http://seedsfeedsandneeds.com/events.html">Events</a></li>
+		<li><a href="http://seedsfeedsandneeds.com/where.html">Where We Meet</a></li>
+		<li><a href="http://www.seedsfeedsandneeds.com/contact1.html">Contact</a></li>
+		<li><a href="http://www.seedsfeedsandneeds.com/volunteer.html">Volunteer</a></li>
+	</ul>
+	
+	
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+		var pageTracker = _gat._getTracker("UA-1856861-4");
+		pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
+
+
 <style>
 
 
@@ -69,13 +124,8 @@ body {
 	background-color: #f5f5f5;
 	}
 </style>
-</head>
 
-<body>
-<center>
-    <div id="wrapper">
 
-	<img src="/img/sfnclogo.jpg"><br/>
     <?=$thankYou ?>
 
     <form method="post" action="contact.php">
