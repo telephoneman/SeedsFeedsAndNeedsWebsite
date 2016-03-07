314a7e3396c6219763b39d2068e45010277d2b03
telephoneman <telephoneman@riseup.net>
2016-03-07 01:04:15 -0600

    Rename contact.php to contact1.php

--- contact1.php
+++ contact1.php
@@ -1,3 +1,21 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="jesse.macdonald.sfo@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?><!DOCTYPE html>
+
 <html lang="en">
 	<meta charset="utf-8" />
 <head>		
@@ -107,6 +125,23 @@ body {
 </head>
 <body>
 <center>
+    <title>Say something!<title/>
+
+    <?=$thankYou ?>
+
+    <form method="post" action="contact.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="senderEmail">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+    
     <div id="wrapper" align="left">
 	<a href="https://www.facebook.com/groups/107127302808643/"><img src="http://seedsfeedsandneeds.org/img/sfnclogo.jpg" title="alt text goes here" align="left"/></a>
 <center>
